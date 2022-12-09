be50605338bef51a26e1847b00997eaf5df34405
Théophile BIHR <User@LAPTOP-A9T0OSJ4.education.lakanal.epf.fr>
2022-12-09 10:24:41 +0100

    création

---
+++ src/speedclick_marchand_bihr/Speedclick_MARCHAND_BIHR.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package speedclick_marchand_bihr;
+
+/**
+ *
+ * @author User
+ */
+public class Speedclick_MARCHAND_BIHR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
