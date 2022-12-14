9eaad8be10561e24bfbc928d2ef6431ef883714b
Théophile BIHR <User@LAPTOP-A9T0OSJ4.education.lakanal.epf.fr>
2022-12-14 09:08:25 +0100

    on est en plein dans la classe parie, quand on clique dans une colone le programme crash

--- src/speedclick_marchand_bihr/matriceDeBoutons.java
+++ src/speedclick_marchand_bihr/matriceDeBoutons.java
@@ -1,5 +1,7 @@
 package speedclick_marchand_bihr;
 
+import java.util.Random;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -12,12 +14,13 @@ public class matriceDeBoutons {
 
     private int ligne;
     private int colone;
-    Bouton[][] matrice ;    //on crée un tab de boutons
+    private Bouton boutonCourant;
+    Bouton[][] matrice;    //on crée un tab de boutons
 
     public matriceDeBoutons(int l, int c) { // prend en compte les dimensions de l'utilisateur   
         ligne = l;
         colone = c;
-        matrice = new Bouton[ligne][colone]; 
+        matrice = new Bouton[ligne][colone];
     }
 
     public void creematrice() {
@@ -40,18 +43,46 @@ public class matriceDeBoutons {
             }
         }
     }
-
+    
+    public void allumagebouton ()  {
+        Random r = new Random();        //  on en allume un au hasard
+        int a = r.nextInt(5);
+        Random b = new Random();
+        int c = b.nextInt(5);
+        matrice[a][c].allumerbouton();
+        
+            
+            
+            
+            
     public void afficherMatrice() {
+        Random r = new Random();        // dès qu'on affiche la matrice de boutons, on en allume un au hasard
+        int a = r.nextInt(5);
+        Random b = new Random();
+        int c = b.nextInt(5);
+        matrice[a][c].allumerbouton();
+        
         for (int x = ligne - 1; x >= 0; x--) {
             for (int y = 0; y < colone; y++) {
                 if (matrice[x][y].onoff() == true) {
                     System.out.print(" ON ");
-                } 
-                else {
+                } else {
                     System.out.print(" off ");
                 }
             }
             System.out.println("");
+
         }
+    }
+
+    public void BoutonClicker() {
+        for (int i = 0; i < ligne; i++) {
+            for (int j = 0; j < colone; j++) {
+                 if (matrice[i][j].clicker()==true && matrice[i][j].onoff()==true);
+                 afficherMatrice();
+                 matrice[i][j].declicker();
+            }
+        }
+        
     }
 }
